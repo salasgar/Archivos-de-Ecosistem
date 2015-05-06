678e6801eb40ba1fb1223215a78f025dcb6e4a7b
Juan Luis Salas García <salasgar@yahoo.es>
2015-05-06 17:12:52 +0200

    Archivos copiados de la wiki del repositorio Ecosystems

---
+++ TO DO list.py
@@ -0,0 +1,21 @@
+"""
+TODO list
+    
+    Crear primera versión de cuadraditos en la GUI, que sirva para hacer debug en el resto del programa (saber si se mueven, etc). (EMILIO) - DONE.
+
+    Crear interfaz con el usuario (EMILIO)
+
+    Crear la gestión de sustancias en el Biotopo (Salas) ***** IN PROGRES!!!!! ***** Estoy en ello, pero tardaré varios días. Cuando esté legible te aviso.
+
+    Crear la gestión de sustancias en los Organismos
+
+    Decidir qué experimento implementamos primero
+
+    Implementar la depredación (¿EMILIO?)
+
+    Implementar la reproducción asexual por bipartición (¿EMILIO?)
+
+    Juntar cosas de initial_settings.py, Experiment.py y ecosystem_mail.py en la clase Experiment (¿EMILIO?)
+    
+    
+"""
